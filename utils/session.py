2084229bb5ac109ac1e8a8a7792f972c67787fa6
iamrita-ai <amritanoob2005@gmail.com>
2026-01-08 16:43:49 +0530

    Update session.py

--- utils/session.py
+++ utils/session.py
@@ -1,5 +1,6 @@
 # User session management for login flow
 user_sessions = {}
+api_sessions = {}  # Store API clients for logged-in users
 
 def set_user_state(user_id, state, data=None):
     """Set user's current state"""
@@ -22,3 +23,24 @@ def update_user_data(user_id, key, value):
     if user_id not in user_sessions:
         user_sessions[user_id] = {'state': None, 'data': {}}
     user_sessions[user_id]['data'][key] = value
+
+def set_api_session(user_id, app_id, api_client):
+    """Store API client for user"""
+    if user_id not in api_sessions:
+        api_sessions[user_id] = {}
+    api_sessions[user_id][app_id] = api_client
+
+def get_api_session(user_id, app_id):
+    """Get stored API client"""
+    if user_id in api_sessions:
+        return api_sessions[user_id].get(app_id)
+    return None
+
+def clear_api_session(user_id, app_id=None):
+    """Clear API session"""
+    if user_id in api_sessions:
+        if app_id:
+            if app_id in api_sessions[user_id]:
+                del api_sessions[user_id][app_id]
+        else:
+            del api_sessions[user_id]
